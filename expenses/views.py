739affeb6ea71ab441ec4fc6d120117f64e92582
Udi Oron <udioron@gmail.com>
2018-12-27 17:35:56 +0200

    added expense form

--- expenses/views.py
+++ expenses/views.py
@@ -1,12 +1,12 @@
 from django import forms
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from expenses.models import Expense
 
 
 def expense_list(request):
     return render(request, "expenses/expense_list.html", {
-        'object_list': Expense.objects.all(),
+        'object_list': Expense.objects.order_by("-date"),
     })
 
 
@@ -18,19 +18,26 @@ def expense_detail(request, pk):
     })
 
 
-class ContactUsForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    age = forms.IntegerField(required=False)
-    message = forms.CharField(widget=forms.Textarea())
+#
+# class ContactUsForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     age = forms.IntegerField(required=False)
+#     message = forms.CharField(widget=forms.Textarea())
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = "__all__"
 
 
 def expense_create(request):
     if request.method == "POST":
-        form = ContactUsForm(request.POST)
+        form = ExpenseForm(request.POST)
         if form.is_valid():
-            assert False, form.cleaned_data
-
-    form = ContactUsForm()
+            form.save()
+            return redirect("expenses:list")
+    else:
+        form = ExpenseForm()
     return render(request, "expenses/expense_form.html", {
         'form': form,
     })
