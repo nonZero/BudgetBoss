88ffa5d8127c596d0759e99e591b438a193e6526
Udi Oron <udioron@gmail.com>
2018-12-27 17:12:55 +0200

    added links from index to detail

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "expenses"
+
 urlpatterns = [
     path('', views.expense_list),
-    path('<int:pk>/', views.expense_detail),
+    path('<int:pk>/',
+         views.expense_detail,
+         name="detail"),
 ]
