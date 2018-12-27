36a8ebfededeef3ceb7956b4e514c3bbc152ef3c
Udi Oron <udioron@gmail.com>
2018-12-27 17:28:55 +0200

    Contact us form

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "expenses"
 
 urlpatterns = [
-    path('', views.expense_list),
+    path('', views.expense_list, name="list"),
+    path('add/', views.expense_create, name="create"),
     path('<int:pk>/',
          views.expense_detail,
          name="detail"),
--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse, Http404
+from django import forms
 from django.shortcuts import render, get_object_or_404
 
 from expenses.models import Expense
@@ -9,13 +9,28 @@ def expense_list(request):
         'object_list': Expense.objects.all(),
     })
 
+
 def expense_detail(request, pk):
-    # try:
-    #     o = Expense.objects.get(pk=pk)
-    # except Expense.DoesNotExist:
-    #     raise Http404()
     o = get_object_or_404(Expense, pk=pk)
 
     return render(request, "expenses/expense_detail.html", {
         'object': o,
+    })
+
+
+class ContactUsForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    age = forms.IntegerField(required=False)
+    message = forms.CharField(widget=forms.Textarea())
+
+
+def expense_create(request):
+    if request.method == "POST":
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            assert False, form.cleaned_data
+
+    form = ContactUsForm()
+    return render(request, "expenses/expense_form.html", {
+        'form': form,
     })
